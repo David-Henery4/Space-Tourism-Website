60b22152b34de6875e83ba23098bf5ac21636d8e
DizzlyD <david4henery00@hotmail.com>
2026-04-01 19:58:34 +0100

    desination-page-inital-setup

---
+++ src/data/destination-data/destinationTabs.ts
@@ -0,0 +1,22 @@
+
+
+const destinationTabs = [
+  {
+    id: 1,
+    name: "moon"
+  },
+  {
+    id: 2,
+    name: "mars"
+  },
+  {
+    id: 3,
+    name: "europa"
+  },
+  {
+    id: 4,
+    name: "titan"
+  },
+]
+
+export default destinationTabs
