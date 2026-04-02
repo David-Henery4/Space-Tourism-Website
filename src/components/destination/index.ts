3c47af528ab4b7959b48af872888b3afd551b8c6
DizzlyD <david4henery00@hotmail.com>
2026-04-02 14:43:29 +0100

    added-destination-page-mobile

---
+++ src/components/destination/index.ts
@@ -0,0 +1,7 @@
+import TextContent from "./TextContent.astro";
+import ImageContent from "./ImageContent.astro";
+
+export {
+  ImageContent,
+  TextContent
+}
---
+++ src/components/destination/text-content/index.ts
@@ -0,0 +1,5 @@
+import Tabs from "./Tabs.astro";
+
+export {
+  Tabs
+}
