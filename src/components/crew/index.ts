0e5d985aeb35134669cafd89eb95f4f23baad06d
DizzlyD <david4henery00@hotmail.com>
2026-04-03 11:43:31 +0100

    mobile-crew-page-layout-added

---
+++ src/components/crew/index.ts
@@ -0,0 +1,9 @@
+import CrewImage from "./CrewImage.astro";
+import CarouselDots from "./CarouselDots.astro";
+import CrewMemberInfo from "./CrewMemberInfo.astro";
+
+export {
+  CrewImage,
+  CarouselDots,
+  CrewMemberInfo
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Header from "./Header.astro";
 import Navigation from "./Navigation.astro";
 import BackgroundImages from "./BackgroundImages.astro";
+import PageSubHeading from "./PageSubHeading.astro";
 
 export {
   Header,
   Navigation,
-  BackgroundImages
+  BackgroundImages,
+  PageSubHeading
 }
