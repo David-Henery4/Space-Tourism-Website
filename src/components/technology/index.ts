bbc8acb83b9dc3f8dcd5e64d0375bc59a46b5344
DizzlyD <david4henery00@hotmail.com>
2026-04-06 19:40:14 +0100

    added-tech-page-mobile-&-tablet-layout

---
+++ src/components/technology/index.ts
@@ -0,0 +1,9 @@
+import TechImage from "./TechImage.astro";
+import ToggleButtons from "./ToggleButtons.astro";
+import TechTextContent from "./TechTextContent.astro";
+
+export {
+  TechImage,
+  TechTextContent,
+  ToggleButtons
+}
